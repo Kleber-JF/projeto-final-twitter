82fc3200040948e7f16a5127cd45f0675ce3cc15
Kleber Fukushima <kleber.jhun@gmail.com>
2024-03-02 12:34:48 -0300

    adding pythonanywhere as host

--- pftwitter/settings.py
+++ pftwitter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'foo' #os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'kleberjhunF.pythonanywhere.com']
 
 
 # Application definition
